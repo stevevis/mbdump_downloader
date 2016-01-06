0e436342b509233352ba627441ec58d0e59a5e46
Steve Viselli <Steve@Steves-MacBook-Pro.local>
2016-01-07 00:34:25 +1100

    Download cover art archive as well

--- lib/mbdump_downloader/config.rb
+++ lib/mbdump_downloader/config.rb
@@ -3,5 +3,10 @@ module MBDumpDownloader
   DATABASE_DIR = '/pub/musicbrainz/data/fullexport'
   DOWNLOAD_TO = '/tmp/dumps'
   LATEST_FILE = 'LATEST'
-  DOWNLOAD_FILES = ['mbdump.tar.bz2', 'mbdump-editor.tar.bz2', 'mbdump-derived.tar.bz2']
+  DOWNLOAD_FILES = [
+    'mbdump.tar.bz2',
+    'mbdump-editor.tar.bz2',
+    'mbdump-derived.tar.bz2',
+    'mbdump-cover-art-archive.tar.bz2'
+  ]
 end
